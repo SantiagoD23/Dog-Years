df6e5749de2941b7f8531d4bfed68db426028cde
SantiagoD23 <123918484+SantiagoD23@users.noreply.github.com>
2023-02-05 20:06:43 -0600

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,16 @@
+//Created a variable of my age
+const myAge = 30;
+// Created a variable of my age 
+let earlyYears = 2;
+// setting the value to earlyYear to 2 
+earlyYears *= 10.5;
+// Created a variable for laterYears
+let laterYears = myAge-2;
+//Mutiplied laterYears by 4 
+laterYears *= 4
+//Created my age in dog years by adding earlyYears and laterYear
+myAgeIndogYears = earlyYears + laterYears;
+//Created variable my name and attached code to make all letters lowercase
+let myname = 'Santiago'.toLowerCase();
+//Used string interpolation to finish the sentence
+console.log(`My name is ${myname}. I am ${myAgeIndogYears} years old in dog years.`); 
